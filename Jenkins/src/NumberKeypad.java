109637aa50a327c7d58995227c761aa4943e911a
Pushan Mukhopadhyay <mukhopadhyaypushan42@gmail.com>
2026-03-12 10:11:42 +0530

    adding Jenkins code

---
+++ Jenkins/src/NumberKeypad.java
@@ -0,0 +1,44 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class NumberKeypad {
+    static void run() {
+        String str = "23";
+        List<String> result = solve(str);
+        System.out.println(result);
+    }
+
+    public static List<String> solve(String str) {
+        String[] mapper = {
+                "",
+                "",
+                "abc",
+                "def",
+                "ghi",
+                "jkl",
+                "mno",
+                "pqrs",
+                "tuv",
+                "wxyz"
+        };
+        List<String> result = new ArrayList<>();
+        backtrack(str, mapper, 0, new StringBuilder(), result);
+        return result;
+    }
+
+    public static void backtrack(String str, String[] mapper, int index, StringBuilder current, List<String> result) {
+        if(index == str.length()) {
+            result.add(current.toString());
+            return;
+        }
+
+        String letters = mapper[str.charAt(index) - '0'];
+        for(char letter : letters.toCharArray()) {
+            current.append(letter);
+            backtrack(str, mapper, index + 1, current, result);
+            current.deleteCharAt(current.length() - 1);
+        }
+
+    }
+
+}
